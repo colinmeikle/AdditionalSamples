6dd1b8e287082be12dcc012c6050e3128eca3427
colinmeikle <colinm@GBL-COLINM2-7.mgc.mentorg.com>
2015-07-11 21:47:42 +0100

    sming template project for netbeans
    sming template

---
+++ build/classes-generated/org/myorg/additionalsamples/Bundle.java
@@ -0,0 +1,13 @@
+package org.myorg.additionalsamples;
+/** Localizable strings for {@link org.myorg.additionalsamples}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>sming project</i>
+     * @see smingExampleWizardIterator
+     */
+    static String smingExample_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "smingExample_displayName");
+    }
+    private void Bundle() {}
+}
